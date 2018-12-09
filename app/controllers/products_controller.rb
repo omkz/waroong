1fd6c6cea06b40c27b2850eaa3705093b7f130dd
Kurnia Muhamad <omenkzz@gmail.com>
2018-12-09 20:48:34 +0700

    Add product's detail page
    Show a product page.
    Make a product form.
    Update shopping cart.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,11 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.includes(image_attachment: :blob)
+    #@order_item = current_order.order_items.new
+  end
+
+  def show
+    @product = Product.find(params[:id])
     @order_item = current_order.order_items.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
       root to: "orders#index"
     end
-  resources :products, only: [:index]
+  resources :products, only: [:index, :show]
   resource :cart, only: [:show]
   resources :order_items, only: [:create, :update, :destroy]
   root to: "products#index"
