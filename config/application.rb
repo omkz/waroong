c762858ac944a2565a863983c0e4baa780a8dd95
Kurnia Muhamad <omenkzz@gmail.com>
2019-08-10 15:57:05 +0700

    Using default variant processor

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,6 @@ module RailsStore
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     #config.load_defaults 6.0
-    config.active_storage.variant_processor = :vips
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
