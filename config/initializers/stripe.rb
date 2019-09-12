943114cde77af643eed71c28886fc22233e51892
Kurnia Muhamad <omenkzz@gmail.com>
2019-09-13 03:22:25 +0700

    Fix typo on stripe variable

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => Rails.application.credentials.aws[:stripe_publishable_key] ,
-  :secret_key      => Rails.application.credentials.aws[:stripe_secret_key]
+  :publishable_key => Rails.application.credentials.stripe[:stripe_publishable_key],
+  :secret_key      => Rails.application.credentials.stripe[:stripe_secret_key]
 }
 
-Stripe.api_key = Rails.application.credentials.aws[:stripe_secret_key]
+Stripe.api_key = Rails.application.credentials.stripe[:stripe_secret_key]
