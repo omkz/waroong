6348e3ac9ccc708676c865a3d835874625758bb3
Kurnia Muhamad <omenkzz@gmail.com>
2019-09-13 03:16:05 +0700

    Using rails credentials

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-    :publishable_key => ENV['PUBLISHABLE_KEY'],
-    :secret_key      => ENV['SECRET_KEY']
-  }
-  
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+  :publishable_key => Rails.application.credentials.aws[:stripe_publishable_key] ,
+  :secret_key      => Rails.application.credentials.aws[:stripe_secret_key]
+}
+
+Stripe.api_key = Rails.application.credentials.aws[:stripe_secret_key]
