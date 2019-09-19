7af757872738cdb2b8aa42bc50bc696073c37658
Kurnia Muhamad <omenkzz@gmail.com>
2019-09-20 03:11:52 +0700

    Update payment using Stripe custom form

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,25 +1,26 @@
 class ChargesController < ApplicationController
-    def new
-    end
-    
-    def create
-      # Amount in cents
-      @amount = current_order.subtotal
-    
-      customer = Stripe::Customer.create(
-        :email => params[:stripeEmail],
-        :source  => params[:stripeToken]
-      )
-    
-      charge = Stripe::Charge.create(
-        :customer    => customer.id,
-        :amount      => (@amount * 100).to_i,
+  def new
+  end
+
+  def create
+    # Amount in cents
+    @amount = current_order.subtotal
+
+    customer = Stripe::Customer.create email: params[:email],
+                                       card: params[:registration][:card_token]
+
+    charge = Stripe::Charge.create(
+        :customer => customer.id,
+        :amount => (@amount * 100).to_i,
         :description => 'Wangoon Stripe customer',
-        :currency    => 'usd'
-      )
-    
-    rescue Stripe::CardError => e
-      flash[:error] = e.message
-      redirect_to new_charge_path
-    end
+        :currency => 'usd'
+    )
+
+    session[:order_id] = nil
+
+  rescue Stripe::CardError => e
+    flash[:notice] = e.message
+    redirect_to new_charge_path
+  end
+
 end
