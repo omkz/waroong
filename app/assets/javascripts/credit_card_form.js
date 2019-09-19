e07f61bd58a9d917ef2c80bdcc6d39cb91b9dbec
Kurnia Muhamad <omenkzz@gmail.com>
2019-09-20 04:12:48 +0700

    Add handler stripe.js error in view

--- app/assets/javascripts/credit_card_form.js
+++ app/assets/javascripts/credit_card_form.js
@@ -1,34 +1,35 @@
 jQuery(function ($) {
-  var show_error, stripeResponseHandler;
-  $("#new_registration").submit(function (event) {
-    var $form;
-    $form = $(this);
-    $form.find("input[type=submit]").prop("disabled", true);
-    Stripe.card.createToken($form, stripeResponseHandler);
-    return false;
-  });
+    var show_error, stripeResponseHandler;
+    $("#new_registration").submit(function (event) {
+        var $form;
+        $form = $(this);
+        $form.find("input[type=submit]").prop("disabled", true);
+        Stripe.card.createToken($form, stripeResponseHandler);
+        return false;
+    });
 
-  stripeResponseHandler = function (status, response) {
-    var $form, token;
-    $form = $("#new_registration");
-    if (response.error) {
-      show_error(response.error.message);
-      $form.find("input[type=submit]").prop("disabled", false);
-    } else {
-      token = response.id;
-      $form.append($("<input type=\"hidden\" name=\"registration[card_token]\" />").val(token));
-      $("[data-stripe=number]").remove();
-      $("[data-stripe=cvv]").remove();
-      $("[data-stripe=exp-year]").remove();
-      $("[data-stripe=exp-month]").remove();
-      $form.get(0).submit();
-    }
-    return false;
-  };
+    stripeResponseHandler = function (status, response) {
+        var $form, token;
+        $form = $("#new_registration");
+        if (response.error) {
+            show_error(response.error.message);
+            $form.find("input[type=submit]").prop("disabled", false);
+        } else {
+            token = response.id;
+            $form.append($("<input type=\"hidden\" name=\"registration[card_token]\" />").val(token));
+            $("[data-stripe=number]").remove();
+            $("[data-stripe=cvv]").remove();
+            $("[data-stripe=exp-year]").remove();
+            $("[data-stripe=exp-month]").remove();
+            $form.get(0).submit();
+        }
+        return false;
+    };
 
-  show_error = function (message) {
-    $("#flash-messages").html('<div class="alert alert-warning"><a class="close" data-dismiss="alert">×</a><div id="flash_alert">' + message + '</div></div>');
-    $('.alert').delay(5000).fadeOut(3000);
-    return false;
-  };
+    show_error = function (message) {
+        $('#stripe_error').text(message)
+        $("#flash-messages").html('<div class="alert alert-warning"><a class="close" data-dismiss="alert">×</a><div id="flash_alert">' + message + '</div></div>');
+        $('.alert').delay(5000).fadeOut(3000);
+        return false;
+    };
 });
