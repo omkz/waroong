69d8c633000c9574f0ceb7e20546b17e95de7d9a
Kurnia Muhamad <omenkzz@gmail.com>
2019-09-21 14:27:24 +0700

    Update stripe.js v2 to stripe elements

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -6,8 +6,8 @@ class ChargesController < ApplicationController
     # Amount in cents
     @amount = current_order.subtotal
 
-    customer = Stripe::Customer.create email: params[:email],
-                                       card: params[:registration][:card_token]
+    customer = Stripe::Customer.create email: params[:"cardholder-email"],
+                                       :source => params[:stripeToken]
 
     charge = Stripe::Charge.create(
         :customer => customer.id,
