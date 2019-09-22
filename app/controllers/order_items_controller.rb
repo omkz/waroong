1d9c6386a45afea5e0ab97d0a04a5e0bfec34aa8
Kurnia Muhamad <omenkzz@gmail.com>
2019-09-22 22:46:32 +0700

    Fix update quantity

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -3,7 +3,7 @@ class OrderItemsController < ApplicationController
   def create
     @order = OrderItem.where("product_id = ? AND order_id = ?", params[:order_item][:product_id], session[:order_id]).first
     if @order
-      @order.increment(:quantity)
+      @order.update(quantity: @order.quantity + params[:order_item][:quantity].to_i)
       @order.save
     else
       @order = current_order
