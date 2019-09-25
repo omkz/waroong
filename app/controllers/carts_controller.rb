8e3868f8f1ea58d2cc8f046ff221feab1d423839
Kurnia Muhamad <omenkzz@gmail.com>
2019-09-26 00:08:08 +0700

    Eager load active storage

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,5 @@
 class CartsController < ApplicationController
   def show
-    @order_items = current_order.order_items.includes(:product)
+    @order_items = current_order.order_items.includes(product: [image_attachment: :blob])
   end
 end
