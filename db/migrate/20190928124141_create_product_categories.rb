5efbfaf2d1d267e916b5446c247430b2a7408406
Kurnia Muhamad <omenkzz@gmail.com>
2019-09-28 19:56:06 +0700

    Fix product categories table scheme
    - Update name attribute to not null.

--- db/migrate/20190928124141_create_product_categories.rb
+++ db/migrate/20190928124141_create_product_categories.rb
@@ -1,7 +1,7 @@
 class CreateProductCategories < ActiveRecord::Migration[6.0]
   def change
     create_table :product_categories do |t|
-      t.string :name
+      t.string :name, null: false
       t.string :description
 
       t.timestamps
