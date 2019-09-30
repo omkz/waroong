29009d7bd8fb3f01f49b8cb5741f7ff242db9f5f
Kurnia Muhamad <omenkzz@gmail.com>
2019-09-30 18:57:38 +0700

    Use eager loading for product dashboard

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,21 +1,9 @@
 module Admin
   class ProductsController < Admin::ApplicationController
-    # To customize the behavior of this controller,
-    # you can overwrite any of the RESTful actions. For example:
-    #
-    # def index
-    #   super
-    #   @resources = Product.
-    #     page(params[:page]).
-    #     per(10)
-    # end
 
-    # Define a custom finder by overriding the `find_resource` method:
-    # def find_resource(param)
-    #   Product.find_by!(slug: param)
-    # end
+    def scoped_resource
+      Product.includes(image_attachment: :blob)
+    end
 
-    # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
-    # for more information
   end
 end
