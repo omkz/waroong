5150a735d4c7ca6ca2fb6c8703b3a15de9a40c03
Kurnia Muhamad <omenkzz@gmail.com>
2021-12-01 09:22:47 +0700

    Update database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,19 @@
+ProductCategory.delete_all
+bikes = ProductCategory.create! name: "bikes"
+ebikes = ProductCategory.create! name: "e-bikes"
+ProductCategory.create! name: "kids bikes & accessories"
+ProductCategory.create! name: "parts"
+ProductCategory.create! name: "bikes accessories"
+ProductCategory.create! name: "clothing & shoes"
+
 Product.delete_all
-Product.create! id: 1, name: "Banana", price: 0.49, active: true
-Product.create! id: 2, name: "Apple", price: 0.29, active: true
-Product.create! id: 3, name: "Carton of Strawberries", price: 1.99, active: true
+Product.create! id: 1, name: "Gazelle Paris C7 HMB", price: 1999, active: true, product_category: bikes
+Product.create! id: 2, name: "Gazelle Espirit HFB", price: 1200, active: true, product_category: ebikes
+Product.create! id: 3, name: "Simplex Amsterdam Heren fiets", price: 900, active: true
 
 OrderStatus.delete_all
 OrderStatus.create! id: 1, name: "In Progress"
 OrderStatus.create! id: 2, name: "Placed"
 OrderStatus.create! id: 3, name: "Shipped"
 OrderStatus.create! id: 4, name: "Cancelled"
+
