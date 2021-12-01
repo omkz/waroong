9725ecd68354799333399f193f88cb12dabe8045
Kurnia Muhamad <omenkzz@gmail.com>
2021-12-01 12:14:44 +0700

    Add a default image on the product

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,10 +7,21 @@ class Product < ApplicationRecord
 
   default_scope { where(active: true) }
 
+  after_commit :add_default_image, on: %i[create update]
+
   def image_header_variant
     variation =
       ActiveStorage::Variation.new(Uploads.resize_to_limit(width: 500, height: -1, blob: image.blob))
     ActiveStorage::Variant.new(image.blob, variation)
+  end
+
+  def add_default_image
+    unless image.attached?
+      image.attach(
+        io: File.open(Rails.root.join('app', 'assets', 'images', 'no-image.png')),
+        filename: 'no_image.png', content_type: 'image/png'
+      )
+    end
   end
 
 end
